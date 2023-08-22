4d462cfdff90d9b8f8dd69a2c2a480c8712cd5a5
Sachin6096 <sachinkumararya29@gmail.com>
2023-08-22 03:10:39 -0700

    check

--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,11 +1,33 @@
 package com.driver;
 
+import edu.emory.mathcs.backport.java.util.PriorityQueue;
+
 public class CurrentAccount extends BankAccount{
+
+
     String tradeLicenseId; //consists of Uppercase English characters only
+
+    public CurrentAccount(String tradeLicenseId) {
+        this.tradeLicenseId = tradeLicenseId;
+    }
+
+    public String getTradeLicenseId() {
+        return tradeLicenseId;
+    }
+
+    public void setTradeLicenseId(String tradeLicenseId) {
+        this.tradeLicenseId = tradeLicenseId;
+    }
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
+        super(name,balance,5000);
+        if(balance < 5000)
+        {
+            throw new Exception("Insufficient Balance");
+        }
 
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
@@ -13,6 +35,11 @@ public class CurrentAccount extends BankAccount{
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
+
+        char[] arr= tradeLicenseId.toCharArray();
+        if(isValid(arr)) return;
+        PriorityQueue<Pair> pq = new PriorityQueue<>(new SortByFreq());
+        int freq[]
 
     }
 
